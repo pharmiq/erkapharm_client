db85d1ae8c01434e9ed6add64d3ff404f29781b4
Sergey Zabolotnov <sergey.zabolotnov@gmail.com>
2021-12-23 09:26:10 +0530

    Describe Webhooks::Models::Appointment

--- lib/erkapharm/webhooks/models/appointment.rb
+++ lib/erkapharm/webhooks/models/appointment.rb
@@ -3,7 +3,33 @@
 module Erkapharm
   module Webhooks
     module Models
+      class Good < Base
+        property :id
+        property :name
+        property :qtty
+        property :sum
+        property :disc
+        property :disc_percent
+        property :mnn
+      end
+
+      class Basket < Base
+        property :rowId, from: :row_id
+        property :qtty_sold, from: :qttySold
+        property :sum_sold, from: :sumSold
+        property :disc_sold, from: :discSold
+        property :disc_percent_sold, from: :discPercentSold
+        property :goods, coerce: Array[Good]
+      end
+
       class Appointment < Base
+        property :request_id, from: :requestId
+        property :order_id, from: :orderId
+        property :doctor_appointment_id, from: :doctorAppointmentId
+        property :order_sum, from: :orderSum
+        property :order_discount, from: :orderDiscount
+        property :order_discount_percent, from: :orderDiscountPercent
+        property :basket, coerce: Array[Basket]
       end
     end
   end
